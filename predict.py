65098f7a44bf7b0e59ce7cb7174a846ec7514c20
Yuki <workcomputer.com>
2023-04-28 15:41:25 +0800

    train flowers finish

--- predict.py
+++ predict.py
@@ -8,30 +8,29 @@ from funcs import draw_mulit_images_in_one
 
 
 if __name__ == "__main__":
-    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    device = (
+        torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    )
 
     # gen model
-    model = Unet(
-        dim = 64,
-        dim_mults = (1, 2, 4, 8)
-    )
+    model = Unet(dim=64, dim_mults=(1, 2, 4, 8))
 
     diffusion = GaussianDiffusion(
         model,
-        image_size = 128,
-        timesteps = 1000,           # number of steps
-        sampling_timesteps = 250,   # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
+        image_size=128,
+        timesteps=1000,  # number of steps
+        sampling_timesteps=250,  # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
     )
 
     # load weight
-    epoch = "90"
-    state_dict: dict = torch.load(f"results/intel-image-classification-scenery/glacier/100000/model-{epoch}.pt")
-    print(state_dict.keys()) # ['step', 'model', 'opt', 'ema', 'scaler', 'version']
+    epoch = "89"
+    state_dict: dict = torch.load(f"results/flowers/100000/model-{epoch}.pt")
+    print(state_dict.keys())  # ['step', 'model', 'opt', 'ema', 'scaler', 'version']
     diffusion.load_state_dict(state_dict["model"])
     diffusion.to(device)
     diffusion.eval()
 
-    return_all_timesteps = False
+    return_all_timesteps = True
 
     # sample image
     with torch.inference_mode():
@@ -41,7 +40,7 @@ if __name__ == "__main__":
     y1 = np.array(y1 * 255, dtype=np.uint8)
 
     if not return_all_timesteps:
-        y1 = np.transpose(y1, [0, 2, 3, 1])     # [B, C, H, W] -> [B, H, W, C]
+        y1 = np.transpose(y1, [0, 2, 3, 1])  # [B, C, H, W] -> [B, H, W, C]
         image = Image.fromarray(draw_mulit_images_in_one(y1, width_repeat=6))
         image.save(f"model-{epoch}.png")
     else:
--- predict_multi_model.py
+++ predict_multi_model.py
@@ -10,22 +10,21 @@ import os
 
 # 遍历全部模型,生成图片
 if __name__ == "__main__":
-    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    device = (
+        torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    )
 
     # gen model
-    model = Unet(
-        dim = 64,
-        dim_mults = (1, 2, 4, 8)
-    )
+    model = Unet(dim=64, dim_mults=(1, 2, 4, 8))
 
     diffusion = GaussianDiffusion(
         model,
-        image_size = 128,
-        timesteps = 1000,           # number of steps
-        sampling_timesteps = 250,   # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
+        image_size=128,
+        timesteps=1000,  # number of steps
+        sampling_timesteps=250,  # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
     )
 
-    model_dir = r"results/intel-image-classification-scenery/glacier/100000/"
+    model_dir = r"results/flowers/100000/"
     model_list = os.listdir(model_dir)
     model_list = [model for model in model_list if model.endswith("pt")]
     for model in model_list:
@@ -43,6 +42,6 @@ if __name__ == "__main__":
         y1 = y.cpu().numpy()
         y1 = np.array(y1 * 255, dtype=np.uint8)
 
-        y1 = np.transpose(y1, [0, 2, 3, 1])     # [B, C, H, W] -> [B, H, W, C]
+        y1 = np.transpose(y1, [0, 2, 3, 1])  # [B, C, H, W] -> [B, H, W, C]
         image = Image.fromarray(draw_mulit_images_in_one(y1, width_repeat=6))
         image.save(os.path.join(model_dir, model[:-2] + "png"))
